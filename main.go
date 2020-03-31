95eced411926f7728a2a48cca7a450f1e126c17c
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-03-31 19:57:52 +0700

    load env files and initiate connection to mongodb

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/khofesh/simple-go-api/setup"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/khofesh/simple-go-api/common"
+	"github.com/khofesh/simple-go-api/setup"
+)
 
 func main() {
+	// load env files
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env files")
+	}
+	var mongoURI string = os.Getenv("MONGO_URI")
+
+	_ = common.InitMongo(mongoURI)
+
 	r := setup.Router()
 
 	r.Run(":8090")
