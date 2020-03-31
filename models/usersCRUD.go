8e27e38389433488b04480c8124780707f76b4df
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-03-31 21:59:22 +0700

    user's crud

---
+++ models/usersCRUD.go
@@ -0,0 +1,13 @@
+package models
+
+// CreateUser : create user
+func CreateUser() {}
+
+// FindOneUser : find a user
+func FindOneUser() {}
+
+// UpdateUser : update user's data
+func UpdateUser() {}
+
+// DeleteUser : delete user's data
+func DeleteUser() {}
