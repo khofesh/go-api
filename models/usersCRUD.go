a76b6f9e6db593c4bc443e2c29e0da4aa3ecdbac
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-04-08 23:13:33 +0700

    change if email already exists and set email to unique

--- models/usersCRUD.go
+++ models/usersCRUD.go
@@ -3,8 +3,9 @@ package models
 import (
 	"context"
 	"fmt"
-	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/khofesh/simple-go-api/common"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -12,23 +13,29 @@ import (
 )
 
 // CreateUser : create user
-func CreateUser(data interface{}) {
+func (u *UserModel) CreateUser(c *gin.Context) error {
 	collection := common.GetCollection("simple", "users")
 
+	if val, _ := collection.CountDocuments(context.TODO(), bson.M{"email": u.Email}); val != 0 {
+		c.JSON(http.StatusOK, gin.H{"message": "Email already exists"})
+	}
+
 	idxMod := mongo.IndexModel{
-		Keys: bson.M{"username": 1}, Options: options.Index().SetUnique(true),
+		Keys: bson.M{"email": 1}, Options: options.Index().SetUnique(true),
 	}
 
 	names, idxerr := collection.Indexes().CreateOne(context.TODO(), idxMod)
 	if idxerr != nil {
-		log.Fatal(idxerr)
+		return idxerr
 	}
 	fmt.Printf("created indexes %v\n", names)
 
-	_, err := collection.InsertOne(context.TODO(), data)
+	_, err := collection.InsertOne(context.TODO(), u)
 	if err != nil {
-		log.Fatal(err.Error())
+		return err
 	}
+
+	return nil
 }
 
 // FindOneUser : find a user
