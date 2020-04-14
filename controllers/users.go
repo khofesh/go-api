98cff9f27245ab6fb1aacb72ac169aa5723310fe
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-04-14 17:03:12 +0700

    test getting data from session

--- controllers/users.go
+++ controllers/users.go
@@ -3,11 +3,14 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 // UserRetrieve :
 // get user's data
 func UserRetrieve(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Success getting user"})
+	session := sessions.Default(c)
+	token := session.Get("sessionData")
+	c.JSON(http.StatusOK, gin.H{"message": "Success getting user", "token": token})
 }
