5695559b7005797fd6d1e181c2933d873fed1783
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-04-14 18:25:40 +0700

    add redis session

--- setup/session.go
+++ setup/session.go
@@ -1,10 +1,12 @@
 package setup
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,5 +15,16 @@ func CookieSession() gin.HandlerFunc {
 	var sessionSecret string = os.Getenv("SESSION_SECRET")
 
 	store := cookie.NewStore([]byte(sessionSecret))
+	return sessions.Sessions("simple_session", store)
+}
+
+// RedisSession : setup redis session
+func RedisSession() gin.HandlerFunc {
+	var sessionSecret string = os.Getenv("SESSION_SECRET")
+
+	store, err := redis.NewStore(10, "tcp", "localhost:6379", "", []byte(sessionSecret))
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 	return sessions.Sessions("simple_session", store)
 }
