9306fdaabe2481ce60316afdfca87748cd19e074
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-04-15 15:13:45 +0700

    get email

--- controllers/users.go
+++ controllers/users.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-contrib/sessions"
@@ -12,7 +11,7 @@ import (
 // get user's data
 func UserRetrieve(c *gin.Context) {
 	session := sessions.Default(c)
-	token := session.Get("user_token")
-	fmt.Println(c.Request)
-	c.JSON(http.StatusOK, gin.H{"message": "Success getting user", "token": token})
+	email := session.Get("user_email")
+
+	c.JSON(http.StatusOK, gin.H{"message": "Success getting user", "email": email})
 }
