ef8a588fe37402147de940dd7f85e300a619d5bc
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-04-15 15:14:39 +0700

    use handlers from jwt middlewares

--- routes/auth.go
+++ routes/auth.go
@@ -3,11 +3,13 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/khofesh/simple-go-api/controllers"
+	"github.com/khofesh/simple-go-api/middlewares"
 )
 
 // AuthRoute : handle route for signing up
 func AuthRoute(router *gin.RouterGroup) {
 	router.POST("/signup", controllers.Signup)
-	router.POST("/login", controllers.Login)
-	router.GET("/logout", controllers.Logout)
+	router.POST("/login", middlewares.JWTMiddleware.LoginHandler)
+	router.GET("/refresh_token", middlewares.JWTMiddleware.RefreshHandler)
+	router.GET("/logout", middlewares.JWTMiddleware.LogoutHandler)
 }
