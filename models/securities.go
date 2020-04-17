9ff923789a40e6cdcb8247f0748053f5df744180
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-04-17 18:11:12 +0700

    change name

--- models/securities.go
+++ models/securities.go
@@ -4,9 +4,9 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // SecuritiesModel ...
 type SecuritiesModel struct {
-	ID          primitive.ObjectID   `bson:"_id" json:"id"`
-	Name        string               `bson:"name" json:"name"`
-	BuyCost     float32              `bson:"buycost" json:"buycost"`
-	SellCost    float32              `bson:"sellcost" json:"sellcost"`
-	Portofolios []primitive.ObjectID `bson:"portofolios" json:"portofolios"`
+	ID       primitive.ObjectID   `bson:"_id" json:"id"`
+	Name     string               `bson:"name" json:"name"`
+	BuyCost  float32              `bson:"buycost" json:"buycost"`
+	SellCost float32              `bson:"sellcost" json:"sellcost"`
+	Stocks   []primitive.ObjectID `bson:"stocks" json:"stocks"`
 }
