220fd633cd166097b0e2a45a55022dadbd3dc9ce
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-04-19 17:11:54 +0700

    add several functions to create, update, and delete stock data

--- models/stock.go
+++ models/stock.go
@@ -1,7 +1,15 @@
 package models
 
 import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/khofesh/simple-go-api/common"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // StockModel ... salah nama, should've been stock
@@ -12,4 +20,56 @@ type StockModel struct {
 	EntityNumber string             `bson:"entityNumber" json:"entityNumber"`
 	Sector       string             `bson:"sector" json:"sector"`
 	Subsector    string             `bson:"subsector" json:"subsector"`
+}
+
+// CreateStock ...
+func (stock *StockModel) CreateStock() error {
+	coll := common.GetCollection("simple", "stock")
+
+	if val, _ := coll.CountDocuments(context.TODO(), bson.M{"entityName": stock.EntityName}); val != 0 {
+		return errors.New("This stock already exists")
+	}
+
+	idxMod := []mongo.IndexModel{
+		{Keys: bson.M{"entityName": 1}, Options: options.Index().SetUnique(true)},
+		{Keys: bson.M{"entityCode": 1}, Options: options.Index().SetUnique(true)},
+		{Keys: bson.M{"entityNumber": 1}, Options: options.Index().SetUnique(true)},
+	}
+
+	names, err := coll.Indexes().CreateMany(context.TODO(), idxMod)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("created indexes %v\n", names)
+
+	_, err = coll.InsertOne(context.TODO(), stock)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateStock ...
+func (stock *StockModel) UpdateStock(update bson.M) error {
+	coll := common.GetCollection("simple", "stock")
+
+	_, err := coll.UpdateOne(context.TODO(), bson.M{"entityName": stock.EntityName}, update)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeleteStock ...
+func (stock *StockModel) DeleteStock() error {
+	coll := common.GetCollection("simple", "stock")
+
+	_, err := coll.DeleteOne(context.TODO(), bson.M{"entityName": stock.EntityName})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
