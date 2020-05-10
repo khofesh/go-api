2d6791379ce4df16c9167b9f2556912ebf8711e1
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-05-10 19:53:54 +0700

    add function to initialize connection to redis and get redit variable

--- common/nosql.go
+++ common/nosql.go
@@ -4,13 +4,18 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"os"
 
+	"github.com/go-redis/redis"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // MoDB : reference to mongodb client
 var MoDB *mongo.Client
+
+// RedisClient ...
+var RedisClient *redis.Client
 
 // InitMongo : initiate connection to mongodb
 func InitMongo(mongoURI string) error {
@@ -42,4 +47,28 @@ func GetMongoDB() *mongo.Client {
 // get collection
 func GetCollection(dbname string, collection string) *mongo.Collection {
 	return MoDB.Database(dbname).Collection(collection)
+}
+
+// InitRedis : initialize connection to redis
+func InitRedis() error {
+	dsn := os.Getenv("REDIS_DSN")
+	if len(dsn) == 0 {
+		dsn = "localhost:6379"
+	}
+
+	RedisClient = redis.NewClient(&redis.Options{
+		Addr: dsn,
+	})
+
+	_, err := RedisClient.Ping().Result()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetRedis ...
+func GetRedis() *redis.Client {
+	return RedisClient
 }
