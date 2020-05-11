7d4a323dea6184744798dc5e5858f5709c95ad54
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-05-11 21:47:41 +0700

    change refresh token route and move /login-admin and /logout-admin to admin.go

--- routes/auth.go
+++ routes/auth.go
@@ -10,9 +10,6 @@ import (
 func AuthRoute(router *gin.RouterGroup) {
 	router.POST("/signup", controllers.Signup)
 	router.POST("/login", middlewares.JWTMiddleware.LoginHandler)
-	router.GET("/refresh_token", middlewares.JWTMiddleware.RefreshHandler)
+	router.GET("/refresh-token", middlewares.JWTMiddleware.RefreshHandler)
 	router.GET("/logout", middlewares.JWTMiddleware.LogoutHandler)
-
-	router.POST("/login-admin", controllers.AdminLogin)
-	router.GET("/logout-admin", middlewares.TokenAuthMiddleware(), controllers.AdminLogout)
 }
