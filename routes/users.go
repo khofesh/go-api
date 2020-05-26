2adf7bfbfc09d562f554dd9633353ad18756c881
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-05-26 17:49:59 +0700

    add auth middleware

--- routes/users.go
+++ routes/users.go
@@ -3,9 +3,10 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/khofesh/simple-go-api/controllers"
+	"github.com/khofesh/simple-go-api/middlewares"
 )
 
 // UserRoute user related routes
 func UserRoute(router *gin.RouterGroup) {
-	router.GET("/test", controllers.UserRetrieve)
+	router.GET("/test", middlewares.JWT.MiddlewareFunc(), controllers.UserRetrieve)
 }
