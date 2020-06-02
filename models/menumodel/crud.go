d934664fc0257758f43aa857debe104c6d5cc04f
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-06-02 22:30:58 +0700

    menu's crud skeleton

---
+++ models/menumodel/crud.go
@@ -0,0 +1,21 @@
+package menumodel
+
+// CreateMenu ...
+func (m *Menu) CreateMenu() error {
+	return nil
+}
+
+// UpdateMenu ...
+func (m *Menu) UpdateMenu() error {
+	return nil
+}
+
+// DeleteMenu ...
+func DeleteMenu() error {
+	return nil
+}
+
+// FindMenus ...
+func FindMenus() ([]Menu, error) {
+	return nil, nil
+}
